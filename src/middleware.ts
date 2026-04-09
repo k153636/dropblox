13a005a1fd1e45e95541c97b63566cc5513d6451
k153636 <o-felice@outlook.jp>
2026-04-09 14:10:36 +0900

    fix: update middleware to make privacy/terms/root public

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|privacy|terms|^$).*)'],
 };
 
 export function middleware(request: NextRequest) {
